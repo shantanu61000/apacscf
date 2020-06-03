c86288cb2a8aa3ddef710e1eec445a4db5c71bf8
shantanu61000 <shantanutripathi96@gmail.com>
2020-06-03 20:36:07 +0530

    scf 1st commit

---
+++ navbar.php
@@ -0,0 +1,15 @@
+                <nav class="navbar navbar-expand-md navbar-light ">
+                    <div class="container">
+                        <a class="navbar-brand" href="#"> <img src="img/APAClogo.png" class="img-logo"> </a>
+                        <button class="navbar-toggler" type="button"  data-toggle="collapse" data-target="#navbarResponse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>                            
+                            <div class="collapse navbar-collapse" style="transition: 1s;" id="navbarResponse" >
+                                <ul class="navbar-nav ml-auto ">
+                                    <li class="nav-item"><a class="nav-link" href="#home">INTRO</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">CONTENT 1</a></li>
+                                    
+                                </ul>
+                            </div>
+                    </div>
+                </nav>
